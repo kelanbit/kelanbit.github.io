a747653c8609e69adaaedc6b4beec803c7f96949
n3v0pr0s <n3v0pr0s@gmail.com>
2020-06-07 19:43:01 +0500

    add folders

--- Service.js
+++
@@ -1,8 +0,0 @@
-function Service(name, alias, value, label) {
-    return {
-        name: name,
-        alias: alias,
-        value: value,
-        label: parseFloat(value).toLocaleString(undefined, { minimumFractionDigits: 2 }) + ' руб.'
-    }
-}
